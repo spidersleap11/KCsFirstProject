1acb1e0d9a66a510b4389e7a9864305a43c123cf
Kelton Hart <HartKC23@pwcs-edu.org>
2018-10-19 14:25:34 -0400

    added text mabye

--- KeltonsFirstForm/KeltonsFirstForm/Form1.vb
+++ KeltonsFirstForm/KeltonsFirstForm/Form1.vb
@@ -10,7 +10,38 @@
     Dim I = 0
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
         Label1.Text = A
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Label2.Text = B
 
     End Sub
 
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Label3.Text = C
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        Label4.Text = D
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+        Label5.Text = e
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        Label6.Text = F
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+        Label7.Text = G
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+        Label8.Text = H
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+        Label9.Text = I
+    End Sub
 End Class
